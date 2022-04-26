961b7b123b6d35ec59da4971204fc0e29bb2c92c
omar-bay <omarmb2002@gmail.com>
2022-04-26 19:15:51 +0300

    building the model layers

--- train.py
+++ train.py
@@ -24,3 +24,27 @@ validation_generator = validation_data_gen.flow_from_directory(
     batch_size=64,
     color_mode='grayscale',
     class_mode='categorical')
+
+# create medel structure
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))   # 7 categories (emotions)
+
+cv2.ocl.setUseOpenCL(False)
+
+emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
+
