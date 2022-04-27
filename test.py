5058db37aa082ce06afd8a11938b5f7104469e78
omar-bay <omarmb2002@gmail.com>
2022-04-27 12:18:54 +0300

    load weights to model from .h5 file

--- test.py
+++ test.py
@@ -9,3 +9,7 @@ json_file = open('model/emotion_model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 emotion_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+emotion_model.load_weights("model/emotion_model.h5")
+print("Loaded model from disk")
